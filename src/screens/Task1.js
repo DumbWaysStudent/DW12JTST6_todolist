20d1f496c75da02d1e8b7453312b7c22d031b740
Irvan Dinda Prakoso <irvandindaprakoso@gmail.com>
2019-10-06 20:51:46 +0700

    add 2.add_todo

--- src/screens/Task1.js
+++ src/screens/Task1.js
@@ -2,27 +2,50 @@ import React, { Component } from 'react';
 import { View, FlatList, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 class Task1 extends Component {
+    constructor(){
+        super();
+        this.state={
+            data:[
+                {name : 'work'},
+                {name : 'swimm'},
+                {name : 'study'},
+                {name : 'sleep'},
+                {name : 'run'},
+            ]
+        }
+    }
+    renderItem = ({item, index})=>{
+        return (
+            <Text style={styles.item} key={index}>
+                {item.name}
+            </Text>
+        )
+    }
+    addList = () => {
+        this.state.data.push({'name': this.state.input})
+        this.setState({input : ''})
+    };
     render(){
-        const activities = [
-            {name : 'work'},
-            {name : 'swimm'},
-            {name : 'study'},
-            {name : 'sleep'},
-            {name : 'run'},
-        ]
         return(
             <View style={styles.container}>
+                <View style={styles.topbar}>
+                    <TextInput
+                        placeholder="New Todo"
+                        value = {this.state.input}
+                        onChangeText = {(text) => {
+                            this.setState({
+                                input: text
+                            })
+                        }}
+                    />
+                    <Button title="Add" onPress={() => this.addList()}/>
+                </View>
                 <View style={styles.container}>
                     <FlatList 
-                        keyExtractor = {activity => activity.name}
-                        data = {activities}
-                        renderItem = {({item})=>{
-                            return (
-                                <Text style={styles.item}>
-                                    {item.name}
-                                </Text>
-                            )
-                        }}
+                        data={this.state.data}
+                        renderItem={this.renderItem}
+                        extraData = {this.state}
+                        
                     />
                 </View>
             </View>
@@ -35,8 +58,13 @@ const styles = StyleSheet.create({
     container: {
         flex:1
     },
+    topbar:{
+        // flex:1,
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        padding:10
+    },
     item:{
-        flex:1,
         padding:10,
         fontSize:18,
         height:44,
